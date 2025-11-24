7043532315138575b30cbd43361b275506e71982
Matt Geiger <et2.geiger@gmail.com>
2025-11-23 22:46:50 -0800

    Model theme switcher after standard mode toggle

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,51 +1,32 @@
 "use client";
 
-import * as React from "react";
-import { Moon, Sun, SunMoon } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
+  DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-const iconMap = {
-  light: Sun,
-  dark: Moon,
-  system: SunMoon,
-};
-
 export function ThemeSwitcher() {
-  const { theme, setTheme } = useTheme();
-  const ThemeIcon = iconMap[(theme as keyof typeof iconMap) ?? "system"] ?? SunMoon;
+  const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className="h-9 w-9">
-          <ThemeIcon className="h-5 w-5" />
+        <Button variant="outline" size="icon" className="relative h-9 w-9">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
-          <DropdownMenuRadioItem value="light">
-            <Sun className="mr-2 h-4 w-4" />
-            Light Mode
-          </DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="dark">
-            <Moon className="mr-2 h-4 w-4" />
-            Dark Mode
-          </DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="system">
-            <SunMoon className="mr-2 h-4 w-4" />
-            Auto (System)
-          </DropdownMenuRadioItem>
-        </DropdownMenuRadioGroup>
+        <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
