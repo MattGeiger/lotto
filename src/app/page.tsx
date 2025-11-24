b9f5cb42006836a6d4ee31d37a5dcf3d4123c4be
Matt Geiger <et2.geiger@gmail.com>
2025-11-23 23:31:05 -0800

    Raise theme switcher to avoid logo overlap on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { ThemeSwitcher } from "@/components/theme-switcher";
 export default function DisplayPage() {
   return (
     <div className="relative">
-      <div className="absolute right-4 top-4 z-50 sm:right-6 sm:top-6 lg:right-8 lg:top-8">
+      <div className="absolute right-4 top-12 z-50 sm:right-6 sm:top-8 lg:right-8 lg:top-10">
         <ThemeSwitcher />
       </div>
       <ReadOnlyDisplay />
